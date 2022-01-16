b950b5e860777f6089f96d6dc38aca8f0ff31d1b
Georgios Doudouliakis <dudugeorge89@gmail.com>
2022-01-16 18:47:02 +0200

    feat: Layout and styling of sign up page

--- src/app/modules/authentication/components/sign-up/sign-up.component.ts
+++ src/app/modules/authentication/components/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
-
-  constructor() { }
+  signupForm: FormGroup;
+  
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.initializeSignupForm();
   }
 
+  get email() {
+    return this.signupForm.get('email');
+  }
+
+  get password() {
+    return this.signupForm.get('password');
+  }
+
+  get confirmPassword() {
+    return this.signupForm.get('confirmPassword');
+  }
+
+  get emailErrorMessage() {
+    if(this.email?.hasError('email')) {
+      return 'Not a valid email';
+    } 
+    return 'Email is required';
+  }
+
+  initializeSignupForm() {
+    this.signupForm = this.fb.group({
+      email: this.fb.control('', [Validators.required, Validators.email]),
+      password: this.fb.control('', [Validators.required, Validators.minLength(10)]),
+      confirmPassword: this.fb.control('', [Validators.required, Validators.minLength(10)])
+    })
+  }
 }
