e706a9affc3abe3a834662a357eba0c389f74603
Georgios Doudouliakis <dudugeorge89@gmail.com>
2022-01-16 17:55:34 +0200

    feat: Connect Sign Up button in login page with sign up page

--- src/app/modules/authentication/components/login/login.component.ts
+++ src/app/modules/authentication/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +10,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, public router: Router) { }
 
   ngOnInit(): void {
     this.initializeLoginForm();
