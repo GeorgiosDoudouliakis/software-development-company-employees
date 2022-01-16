214e74b3f1bae9f0382c89b1e94cea03a140e1f1
Georgios Doudouliakis <dudugeorge89@gmail.com>
2022-01-17 01:34:06 +0200

    feat: Sign in user

--- src/app/modules/authentication/components/auth/auth.component.ts
+++ src/app/modules/authentication/components/auth/auth.component.ts
@@ -3,6 +3,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { ForgotPasswordDialogComponent } from '../forgot-password-dialog/forgot-password-dialog.component';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { FirebaseError } from '@firebase/util';
 
 @Component({
   selector: 'app-auth',
@@ -16,7 +19,13 @@ export class AuthComponent implements OnInit {
   hidePassword: boolean = true;
   hideConfirmPassword: boolean = true;
 
-  constructor(private fb: FormBuilder, public router: Router, private dialog: MatDialog) { }
+  constructor(
+    private fb: FormBuilder, 
+    public router: Router, 
+    private dialog: MatDialog,
+    private afAuth: AngularFireAuth,
+    private _snackBar: MatSnackBar
+  ) { }
 
   ngOnInit(): void {
     this.initializeForm();
@@ -58,5 +67,34 @@ export class AuthComponent implements OnInit {
     });
 
     dialogRef.afterClosed().subscribe();
+  }
+
+  onSubmit() {
+    const email = this.email?.value;
+    const password = this.password?.value;
+
+    if(this.authType === 'signup' 
+    && this.authForm.valid 
+    && this.password?.value === this.confirmPassword?.value) {
+      this.afAuth.createUserWithEmailAndPassword(email, password)
+        .then(_ => this.openSnackBar('You have successfully signed in!', 'success'))
+        .catch((err: FirebaseError) => this.openSnackBar(this.authenticationError(err.message), 'error'));
+    }
+  }
+
+  private openSnackBar(message: string, className: string) {
+    this._snackBar.open(message, 'OK', {
+      horizontalPosition: 'end',
+      verticalPosition: 'bottom',
+      panelClass: className
+    });
+  }
+
+  private authenticationError(error: string) {
+    // Firebase error is something like: Firebase: Password should be at least 6 characters (auth/weak-password)
+    const slash = error.indexOf('/');
+    const closingParenthesis = error.indexOf(')');
+    const errorMessage = error.substring(slash + 1, closingParenthesis).split('-').join(' ');
+    return errorMessage.charAt(0).toUpperCase() + errorMessage.slice(1);
   }
 }
