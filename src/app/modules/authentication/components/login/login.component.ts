1206f09aaca17dfeefe7aa56c9729fa19656943c
Georgios Doudouliakis <dudugeorge89@gmail.com>
2022-01-16 17:23:57 +0200

    feat: Layout and styling of login page

--- src/app/modules/authentication/components/login/login.component.ts
+++ src/app/modules/authentication/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.initializeLoginForm();
   }
 
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  get passwordErrorMessage() {
+    if(this.password?.hasError('email')) {
+      return 'Not a valid email';
+    } 
+    return 'Password must contain at least 10 characters';
+  }
+
+  initializeLoginForm() {
+    this.loginForm = this.fb.group({
+      email: this.fb.control('', [Validators.required]),
+      password: this.fb.control('', [Validators.required, Validators.email,  Validators.minLength(10)])
+    })
+  }
 }
