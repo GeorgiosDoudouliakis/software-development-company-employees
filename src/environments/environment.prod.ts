d3181e511f89b88c453c4a0d5dda5158de233a65
Georgios Doudouliakis <dudugeorge89@gmail.com>
2022-01-16 23:15:18 +0200

    feat: Add angular fire package

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'software-dev-company-employees',
+    appId: '1:724246069578:web:12b47dbfa8d3b5c2acc1b3',
+    storageBucket: 'software-dev-company-employees.appspot.com',
+    locationId: 'europe-west6',
+    apiKey: 'AIzaSyBFjVOjgfOzPx3v_k35IjB8hxi7ebiS9Tc',
+    authDomain: 'software-dev-company-employees.firebaseapp.com',
+    messagingSenderId: '724246069578',
+  },
   production: true
 };
