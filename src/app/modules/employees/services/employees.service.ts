bebc8e80d592d14786ced2a2874b4dbe3e356a51
Georgios Doudouliakis <dudugeorge89@gmail.com>
2022-01-18 00:58:41 +0200

    feat: Add method on employees service for getting all the employees of the company

--- src/app/modules/employees/services/employees.service.ts
+++ src/app/modules/employees/services/employees.service.ts
@@ -3,6 +3,7 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Employee } from '../models/employee.model';
+import { switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -21,6 +22,19 @@ export class EmployeesService {
       ...data,
       uid: user?.uid
     })
+  }
+
+  getEmployees() {
+    return this.afAuth.authState.pipe(
+      switchMap((user: any) => {
+        if(user) {
+          return this.db.collection('employees', (ref: any) => ref.where('uid', '==', user.uid))
+                     .valueChanges({ idField: 'id' });
+        } else {
+          return [];
+        }
+      })
+    );
   }
 
   openSnackBar(message: string, className: string) {
