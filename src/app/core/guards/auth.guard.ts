afcc0bf7c1b2a4d9e3cda11d82d1f1f7d78e542e
Georgios Doudouliakis <dudugeorge89@gmail.com>
2022-01-17 21:46:35 +0200

    fix: Auth guard

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { ActivatedRoute, ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
@@ -19,13 +20,13 @@ export class AuthGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Promise<boolean> {
       const user = await this.afAuth.currentUser;
+      const isLoggedin = !!user;
 
-      if(!user) {
+      if(!isLoggedin) {
         this.router.navigate(['/'], { relativeTo: this.route });
         this.authService.openSnackBar('You have no permission to access this link. Please log in!', 'error');
-        return false;
       }
       
-      return true;
+      return isLoggedin;
   }
 }
