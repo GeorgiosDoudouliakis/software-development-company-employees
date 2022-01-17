e68f4556c98fb77731d0ee758ee76369af29be6e
Georgios Doudouliakis <dudugeorge89@gmail.com>
2022-01-18 01:33:59 +0200

    feat: Update Employee model

--- src/app/modules/employees/models/employee.model.ts
+++ src/app/modules/employees/models/employee.model.ts
@@ -3,11 +3,12 @@ export type ContractType = 'full-time' | 'part-time' | 'freelance';
 export type Speciality = 'front-end' | 'back-end' | 'full-stack';
 
 export interface Employee {
-    firstName: string;
-    lastName: string;
-    age: number;
-    contractType: ContractType;
-    speciality: Speciality;
-    hireDate: string;
-    projects: string[];
+    firstName?: string;
+    lastName?: string;
+    age?: number;
+    contractType?: ContractType;
+    speciality?: Speciality;
+    hireDate?: string;
+    projects?: string[];
+    id?: string;
 }
