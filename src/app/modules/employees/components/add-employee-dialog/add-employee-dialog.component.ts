1b09e68ffe24aaf7f514fd821c2ab9e9425645b7
Georgios Doudouliakis <dudugeorge89@gmail.com>
2022-01-18 17:10:42 +0200

    refactor: Use FormGroup instead of ngModel in add employee dialog

--- src/app/modules/employees/components/add-employee-dialog/add-employee-dialog.component.ts
+++ src/app/modules/employees/components/add-employee-dialog/add-employee-dialog.component.ts
@@ -3,6 +3,7 @@ import { MatChipInputEvent } from '@angular/material/chips';
 import { COMMA, ENTER } from '@angular/cdk/keycodes';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Employee } from '../../models/employee.model';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-employee-dialog',
@@ -10,16 +11,48 @@ import { Employee } from '../../models/employee.model';
   styleUrls: ['./add-employee-dialog.component.scss']
 })
 export class AddEmployeeDialogComponent implements OnInit {
+  employeeForm: FormGroup;
   addOnBlur = true;
   projects: string[] = [];
   readonly separatorKeysCodes = [ENTER, COMMA];
 
   constructor(
+    private fb: FormBuilder,
     @Inject(MAT_DIALOG_DATA) public data: Employee, 
     public dialogRef: MatDialogRef<AddEmployeeDialogComponent>
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initializeEmployeeForm();
+  }
+
+  get firstName() {
+    return this.employeeForm.get('firstName');
+  }
+
+  get lastName() {
+    return this.employeeForm.get('lastName');
+  }
+
+  get age() {
+    return this.employeeForm.get('age');
+  }
+
+  get contractType() {
+    return this.employeeForm.get('contractType');
+  }
+
+  get speciality() {
+    return this.employeeForm.get('speciality');
+  }
+  
+  get hireDate() {
+    return this.employeeForm.get('hireDate');
+  }
+
+  get empProjects() {
+    return this.employeeForm.get('projects');
+  }
 
   add(event: MatChipInputEvent): void {
     const value = (event.value || '').trim();
@@ -41,13 +74,25 @@ export class AddEmployeeDialogComponent implements OnInit {
 
   onAddEmployee() {
     this.dialogRef.close({
-      firstName: this.data.firstName,
-      lastName: this.data.lastName,
-      age: this.data.age,
-      contractType: this.data.contractType,
-      speciality: this.data.speciality,
-      hireDate: this.data.hireDate,
-      projects: this.projects
+      firstName: this.firstName?.value,
+      lastName: this.lastName?.value,
+      age: this.age?.value,
+      contractType: this.contractType?.value,
+      speciality: this.speciality?.value,
+      hireDate: this.hireDate?.value,
+      projects: this.empProjects?.value
+    })
+  }
+
+  private initializeEmployeeForm() {
+    this.employeeForm = this.fb.group({
+      firstName: this.fb.control('', Validators.required),
+      lastName: this.fb.control('', Validators.required),
+      age: this.fb.control('', Validators.required),
+      contractType: this.fb.control('', Validators.required),
+      speciality: this.fb.control('', Validators.required),
+      hireDate: this.fb.control('', Validators.required),
+      projects: this.fb.control([], Validators.required),
     })
   }
 }
