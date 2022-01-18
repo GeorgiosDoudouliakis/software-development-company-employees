8039f8899a62fcd0eb8b21e6e58926d176da0e08
Georgios Doudouliakis <dudugeorge89@gmail.com>
2022-01-18 17:35:32 +0200

    feat: On edit employee open add employee dialog with filled fields

--- src/app/modules/employees/components/add-employee-dialog/add-employee-dialog.component.ts
+++ src/app/modules/employees/components/add-employee-dialog/add-employee-dialog.component.ts
@@ -1,6 +1,4 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MatChipInputEvent } from '@angular/material/chips';
-import { COMMA, ENTER } from '@angular/cdk/keycodes';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Employee } from '../../models/employee.model';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
@@ -12,9 +10,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class AddEmployeeDialogComponent implements OnInit {
   employeeForm: FormGroup;
-  addOnBlur = true;
-  projects: string[] = [];
-  readonly separatorKeysCodes = [ENTER, COMMA];
+  availableProjects: string[] = ['Tomi Systems', 'Pure Profile', 'VMWare'];
 
   constructor(
     private fb: FormBuilder,
@@ -46,30 +42,8 @@ export class AddEmployeeDialogComponent implements OnInit {
     return this.employeeForm.get('speciality');
   }
   
-  get hireDate() {
-    return this.employeeForm.get('hireDate');
-  }
-
   get empProjects() {
     return this.employeeForm.get('projects');
-  }
-
-  add(event: MatChipInputEvent): void {
-    const value = (event.value || '').trim();
-
-    if (value) {
-      this.projects.push(value);
-    }
-
-    event.chipInput!.clear();
-  }
-
-  remove(project: string): void {
-    const index = this.projects.indexOf(project);
-
-    if (index >= 0) {
-      this.projects.splice(index, 1);
-    }
   }
 
   onAddEmployee() {
@@ -79,20 +53,22 @@ export class AddEmployeeDialogComponent implements OnInit {
       age: this.age?.value,
       contractType: this.contractType?.value,
       speciality: this.speciality?.value,
-      hireDate: this.hireDate?.value,
       projects: this.empProjects?.value
     })
   }
 
   private initializeEmployeeForm() {
     this.employeeForm = this.fb.group({
-      firstName: this.fb.control('', Validators.required),
-      lastName: this.fb.control('', Validators.required),
-      age: this.fb.control('', Validators.required),
-      contractType: this.fb.control('', Validators.required),
-      speciality: this.fb.control('', Validators.required),
-      hireDate: this.fb.control('', Validators.required),
-      projects: this.fb.control([], Validators.required),
+      firstName: this.fb.control(this.data.firstName || '', Validators.required),
+      lastName: this.fb.control(this.data.lastName || '', Validators.required),
+      age: this.fb.control(this.data.age || '', Validators.required),
+      contractType: this.fb.control(this.data.contractType || '', Validators.required),
+      speciality: this.fb.control(this.data.speciality || '', Validators.required),
+      projects: this.fb.control(this.data.projects || [], Validators.required),
     })
+  }
+
+  private initializeProjects() {
+
   }
 }
--- src/app/modules/employees/components/employee/employee.component.ts
+++ src/app/modules/employees/components/employee/employee.component.ts
@@ -1,7 +1,9 @@
 import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { FirebaseError } from '@firebase/util';
 import { Employee } from '../../models/employee.model';
 import { EmployeesService } from '../../services/employees.service';
+import { AddEmployeeDialogComponent } from '../add-employee-dialog/add-employee-dialog.component';
 
 @Component({
   selector: 'app-employee',
@@ -12,7 +14,7 @@ import { EmployeesService } from '../../services/employees.service';
 export class EmployeeComponent implements OnInit {
   @Input() employee: Employee;
 
-  constructor(private employeesService: EmployeesService) { }
+  constructor(private employeesService: EmployeesService, private dialog: MatDialog) { }
 
   ngOnInit(): void {}
 
@@ -20,6 +22,15 @@ export class EmployeeComponent implements OnInit {
     this.employeesService.deleteEmployee(employeeId)
       .then(_ => this.employeesService.openSnackBar('Employee has been successfully deleted!', 'success'))
       .catch((err: FirebaseError) => this.showErrorMessage(err))
+  }
+
+  openEdit(employee: Employee) {
+    const { firstName, lastName, age, contractType, speciality, projects } = employee;
+
+    this.dialog.open(AddEmployeeDialogComponent, {
+      width: '400px',
+      data: { firstName, lastName, age, contractType, speciality, projects }
+    })
   }
 
   private showErrorMessage(err: FirebaseError) {
