2d1343dfb53cc0caef280af65cfde85671c6e4b5
Georgios Doudouliakis <dudugeorge89@gmail.com>
2022-01-19 22:44:19 +0200

    feat: Add the properties needed in company profile component's ts, retrieve company's details and add method for clearing the fields in ui

--- src/app/modules/company-profile/components/company-profile/company-profile.component.ts
+++ src/app/modules/company-profile/components/company-profile/company-profile.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { GetCompanyService } from '@shared/services/get-company.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-company-profile',
   templateUrl: './company-profile.component.html',
   styleUrls: ['./company-profile.component.scss']
 })
-export class CompanyProfileComponent implements OnInit {
+export class CompanyProfileComponent implements OnInit, OnDestroy {
+  name: string;
+  founder: string;
+  description: string;
+  services: string[] = [];
+  projects: string[] = [];
+  logo: string;
+  private companySub$: Subscription;
 
-  constructor() { }
+  constructor(private getCompanyService: GetCompanyService) { }
 
   ngOnInit(): void {
+    this.companySub$ = this.getCompanyService.company.subscribe((company: any) => {
+       const { name, founder, description, services, projects, logo } = company;
+       this.name = name;
+       this.founder = founder;
+       this.description = description;
+       this.services = services;
+       this.projects = projects;
+       this.logo = logo;
+    });
   }
 
+  ngOnDestroy() {
+    this.companySub$?.unsubscribe();
+  }
+
+  clearFields() {
+    this.name = '';
+    this.founder = '';
+    this.description = '';
+    this.services = [];
+    this.projects = [];
+    this.logo = '';
+  }
 }
