50d1bfa691ad3f7ef92179f7ef58dd02cf98282e
Georgios Doudouliakis <dudugeorge89@gmail.com>
2022-01-19 22:42:32 +0200

    feat: Add mdel for company

---
+++ src/app/shared/models/company.model.ts
@@ -0,0 +1,9 @@
+export interface Company {
+    name?: string;
+    founder?: string;
+    description?: string;
+    services?: string[];
+    projects?: string[];
+    logo?: string;
+    id?: string;
+}
