7d05ecfff811c2a85bc47d50b50dee986494d3b3
Georgios Doudouliakis <dudugeorge89@gmail.com>
2022-01-20 20:30:52 +0200

    feat: Use AuthGuard in company profile module

--- src/app/modules/company-profile/company-profile.module.ts
+++ src/app/modules/company-profile/company-profile.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '@shared/shared.module';
 import { CompanyProfileComponent } from './components/company-profile/company-profile.component';
 import { AddServiceProjectDialogComponent } from './components/add-service-project-dialog/add-service-project-dialog.component';
+import { AuthGuard } from 'src/app/core/guards/auth.guard';
 
 const routes = [
-  { path: '', component: CompanyProfileComponent }
+  { path: '', component: CompanyProfileComponent, canActivate: [AuthGuard] }
 ]
 
 @NgModule({
